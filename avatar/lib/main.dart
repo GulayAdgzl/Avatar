f3156ec34b0e6e670d3b41681bec2e188d281e43
gulay.adiguzel <gulay.adiguzel@peakup.org>
2025-01-06 20:41:01 +0300

    update

--- avatar/lib/main.dart
+++ avatar/lib/main.dart
@@ -2,13 +2,30 @@
 import 'package:avatar/features/home/home_screen.dart';
 import 'package:avatar/features/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 // lib/main.dart
 void main() {
+  WidgetsBinding widgetsBinding=WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState(){
+super.initState();
+initialization();
+  }
+  void initialization() async {
+    await Future.delayed(Duration(seconds: 3));
+    FlutterNativeSplash.remove();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
